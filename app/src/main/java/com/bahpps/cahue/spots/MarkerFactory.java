d41f3c53ab0af226d5c195e2c3cb65cc0c781150
Francesco <f.gatti@speexx.com>
2015-03-05 00:08:19 +0100

    Markers in progress. Need to set markers for different resolutions

--- app/src/main/java/com/bahpps/cahue/spots/MarkerFactory.java
+++ app/src/main/java/com/bahpps/cahue/spots/MarkerFactory.java
@@ -6,75 +6,109 @@ import android.graphics.Canvas;
 import android.graphics.drawable.GradientDrawable;
 
 import com.bahpps.cahue.R;
-import com.bahpps.cahue.spots.ParkingSpot;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.ui.IconGenerator;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by Francesco on 04/11/2014.
  */
 public class MarkerFactory {
 
-    public static IconGenerator iconGenerator;
+    private static long GREEN_TIME_THRESHOLD_MS = 5 * 60 * 1000;
+    private static long YELLOW_TIME_THRESHOLD_MS = 10 * 60 * 1000;
 
-    private static Map<ParkingSpot.Type, BitmapDescriptor> typeBitmapDescriptorMap = new HashMap<ParkingSpot.Type, BitmapDescriptor>();
-    private static Map<ParkingSpot.Type, BitmapDescriptor> selectedTypeBitmapDescriptorMap = new HashMap<ParkingSpot.Type, BitmapDescriptor>();
+    private static IconGenerator iconGenerator;
 
-    public static BitmapDescriptor getMarkerBitmap(ParkingSpot.Type type, Context context, boolean selected) {
+    public static MarkerOptions getMarker(ParkingSpot spot, Context context, boolean selected) {
 
         if (iconGenerator == null) {
             iconGenerator = new IconGenerator(context.getApplicationContext());
         }
 
-        if (selected) {
-
-            BitmapDescriptor descriptor = selectedTypeBitmapDescriptorMap.get(type);
-            if (descriptor == null) {
-                descriptor = createMarkerBitmap(context, type, true);
-                selectedTypeBitmapDescriptorMap.put(type, descriptor);
-            }
-            return descriptor;
-
-        } else {
-
-            BitmapDescriptor descriptor = typeBitmapDescriptorMap.get(type);
-            if (descriptor == null) {
-                descriptor = createMarkerBitmap(context, type, false);
-                typeBitmapDescriptorMap.put(type, descriptor);
-            }
-            return descriptor;
-
-        }
+        long timeSinceSpotWasFree = System.currentTimeMillis() - spot.time.getTime();
+        if (timeSinceSpotWasFree < GREEN_TIME_THRESHOLD_MS)
+            return getGreenMarker(context, timeSinceSpotWasFree, spot.position, selected);
+        else if (timeSinceSpotWasFree < YELLOW_TIME_THRESHOLD_MS)
+            return getYellowMarker(context, timeSinceSpotWasFree, spot.position, selected);
+        else
+            return getRedMarker(context, spot.position, selected);
 
     }
 
-    private static BitmapDescriptor createMarkerBitmap(Context context, ParkingSpot.Type type, boolean selected) {
+    private static MarkerOptions getGreenMarker(Context context, long timeSinceSpotWasFree, LatLng position, boolean selected) {
+        if(selected)
+            return getSelectedMarker(context, timeSinceSpotWasFree, position);
 
-        iconGenerator.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Inverse);
+        iconGenerator.setTextAppearance(R.style.MarkerStyle);
+        iconGenerator.setBackground(context.getResources().getDrawable(R.drawable.map_marker_green));
+        return getMarkerOptions(timeSinceSpotWasFree, position);
+    }
 
-        if (type == ParkingSpot.Type.green) {
-            iconGenerator.setBackground(context.getResources().getDrawable(R.drawable.map_marker_green));
-            return BitmapDescriptorFactory.fromBitmap(iconGenerator.makeIcon("T"));
-        } else if (type == ParkingSpot.Type.yellow) {
-            iconGenerator.setBackground(context.getResources().getDrawable(R.drawable.map_marker_yellow));
-            return BitmapDescriptorFactory.fromBitmap(iconGenerator.makeIcon("T"));
+    private static MarkerOptions getMarkerOptions(long timeSinceSpotWasFree, LatLng position) {
+        return new MarkerOptions()
+                .position(position)
+                .icon(BitmapDescriptorFactory.fromBitmap(iconGenerator.makeIcon(getMarkerText(timeSinceSpotWasFree))))
+                .anchor(MarkerFactory.iconGenerator.getAnchorU(), MarkerFactory.iconGenerator.getAnchorV());
+    }
+
+    private static MarkerOptions getYellowMarker(Context context, long timeSinceSpotWasFree, LatLng position, boolean selected) {
+        if(selected)
+            return getSelectedMarker(context, timeSinceSpotWasFree, position);
+
+        iconGenerator.setTextAppearance(R.style.MarkerStyle);
+        iconGenerator.setBackground(context.getResources().getDrawable(R.drawable.map_marker_yellow));
+        return getMarkerOptions(timeSinceSpotWasFree, position);
+    }
+
+    private static MarkerOptions getSelectedMarker(Context context, long timeSinceSpotWasFree, LatLng position) {
+        iconGenerator.setTextAppearance(R.style.SelectedMarkerStyle);
+        iconGenerator.setBackground(context.getResources().getDrawable(R.drawable.map_marker_selected));
+        return getMarkerOptions(timeSinceSpotWasFree, position);
+    }
+
+    private static String getMarkerText(long ms) {
+        return String.format("%d'", ms / 60000);
+    }
+
+    private static BitmapDescriptor redBitmap;
+    private static BitmapDescriptor redBitmapSelected;
+
+    private static MarkerOptions getRedMarker(Context context, LatLng position, boolean selected) {
+
+        BitmapDescriptor bitmap;
+        if (redBitmap == null) {
+            redBitmap = createRedMarkerBitmap(context, false);
+        }
+        if (redBitmapSelected == null) {
+            redBitmapSelected = createRedMarkerBitmap(context, true);
         }
 
+        if (selected)
+            bitmap = redBitmapSelected;
+        else
+            bitmap = redBitmap;
 
-        int diameter = context.getResources().getDimensionPixelSize(selected ? R.dimen.marker_diameter_selected : type.diameterId);
+        return new MarkerOptions()
+                .flat(true)
+                .position(position)
+                .icon(bitmap)
+                .anchor(0.5F, 0.5F);
+    }
+
+    private static BitmapDescriptor createRedMarkerBitmap(Context context, boolean selected) {
+        int diameter = context.getResources().getDimensionPixelSize(selected ? R.dimen.marker_diameter_selected : R.dimen.marker_diameter_red);
         Bitmap mDotMarkerBitmap = Bitmap.createBitmap(diameter, diameter, Bitmap.Config.ARGB_8888);
 
         Canvas canvas = new Canvas(mDotMarkerBitmap);
 
-        GradientDrawable drawable = (GradientDrawable) context.getResources().getDrawable(selected ? R.drawable.marker_selected : R.drawable.marker);
+        GradientDrawable drawable = (GradientDrawable) context.getResources().getDrawable(selected ? R.drawable.marker_red_selected : R.drawable.marker_red);
         drawable.setBounds(0, 0, mDotMarkerBitmap.getWidth(), mDotMarkerBitmap.getHeight());
-        drawable.setColor(context.getResources().getColor(type.colorId));
         drawable.draw(canvas);
 
         return BitmapDescriptorFactory.fromBitmap(mDotMarkerBitmap);
     }
+
 }
