de3332bc082dbb4b2ccc5ff5812adb9e1edf57a5
fgatti675 <francesco.gatti.gomez@stylight.com>
2015-07-08 18:09:52 +0200

    Preferences init

---
+++ app/src/main/java/com/cahue/iweco/IwecoPreferencesActivity.java
@@ -0,0 +1,57 @@
+package com.cahue.iweco;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Button;
+
+import java.util.List;
+
+/**
+ * Created by f.gatti.gomez on 08/07/15.
+ */
+public class IwecoPreferencesActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Add a button to the header list.
+        if (hasHeaders()) {
+            Button button = new Button(this);
+            button.setText("Some action");
+            setListFooter(button);
+        }
+    }
+
+    /**
+     * Populate the activity with the top-level headers.
+     */
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.preference_headers, target);
+    }
+
+    /**
+     * This fragment shows the preferences for the first header.
+     */
+    public static class Prefs1Fragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Make sure default values are applied.  In a real app, you would
+            // want this in a shared function that is used to retrieve the
+            // SharedPreferences wherever they are needed.
+//            PreferenceManager.setDefaultValues(getActivity(),
+//                    R.xml.advanced_preferences, false);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.fragmented_preferences);
+        }
+    }
+
+}
+
